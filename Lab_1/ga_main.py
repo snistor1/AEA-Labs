48386a726fccc800d650f61b13cde36a5a36ca10
claudiu-ghiga <claudiu.ghiga@gmail.com>
2020-12-05 18:14:57 +0200

    Separate hyperparameters + live plotting + restructuring

--- Lab_1/ga_main.py
+++ Lab_1/ga_main.py
@@ -1,13 +1,24 @@
 import time
 import copy
-import numpy as np
+import itertools
+import logging
+import multiprocessing as mproc
 
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from selection.strategies import Strategy, RankBased, Roulette
+from evaluation import evaluate
+from operators import upgrade, upgrade_input
+from utils import PlotContext
+from utils import collect_and_log_fitness, setup_logging
 from ga_defines import *
 
 
 def generate_population() -> list:
     population = list()
-    for _ in range(POP_SIZE):
+    for _ in range(NET_POP_SIZE):
         nr_comparators = np.random.randint(3, 20)
         network = list()
         for _ in range(nr_comparators):
@@ -21,30 +32,7 @@ def generate_population() -> list:
 
 
 def generate_input_population() -> list:
-    return [np.random.randint(2, size=NETWORK_SIZE) for _ in range(INPUT_POP_SIZE)]
-
-
-def eval_input(network, input_test_case) -> bool:
-    for comparator in network:
-        # if input_test_case[comparator[0]] > input_test_case[comparator[1]]:
-        input_test_case[[comparator[0], comparator[1]]] = input_test_case[[comparator[1], comparator[0]]]
-    return np.all(input_test_case[:-1] <= input_test_case[1:])
-
-
-def fitness_networks(population: list, input_population: list) -> list:
-    fitness_values = list()
-    for network in population:
-        fitness = sum([eval_input(network, test_case) for test_case in input_population]) / len(input_population)
-        fitness_values.append(fitness)
-    return fitness_values
-
-
-def fitness_input(input_population: list, population: list) -> list:
-    fitness_values = list()
-    for test_case in input_population:
-        fitness = 1 - sum([eval_input(network, test_case) for network in population]) / len(population)
-        fitness_values.append(fitness)
-    return fitness_values
+    return [np.random.randint(2, size=NETWORK_SIZE) for _ in range(IN_POP_SIZE)]
 
 
 def get_best_individual(population, fitness_values) -> (float, list):
@@ -54,134 +42,76 @@ def get_best_individual(population, fitness_values) -> (float, list):
     return best_val, best_individual
 
 
-def upgrade(population: list) -> list:
-    new_population = list()
-    probabilities = np.cumsum(MUTATION_CASES)
-    for individual in population:
-        r = np.random.rand()
-        if r < MUTATION_PROB:
-            mutation_procedure = np.random.rand()
-            selected = np.searchsorted(probabilities, mutation_procedure)
-            if selected == 0:
-                first = np.random.randint(len(individual))
-                second = np.random.choice([i for i in range(len(individual)) if i != first])
-                f0 = individual[first][0]
-                f1 = individual[first][1]
-                s0 = individual[second][0]
-                s1 = individual[second][1]
-                individual[first] = (s0, f1) if s0 <= f1 else (f1, s0)
-                individual[second] = (f0, s1) if f0 <= s1 else (s1, f0)
-            elif selected == 1:
-                if len(individual) < 4:
-                    first = np.random.randint(NETWORK_SIZE)
-                    second = np.random.choice([i for i in range(NETWORK_SIZE) if i != first])
-                    if first > second:
-                        first, second = second, first
-                    index_to_add = np.random.randint(len(individual) + 1)
-                    individual.insert(index_to_add, (first, second))
-                else:
-                    to_remove = np.random.randint(len(individual))
-                    del individual[to_remove]
-            else:
-                first = np.random.randint(NETWORK_SIZE)
-                second = np.random.choice([i for i in range(NETWORK_SIZE) if i != first])
-                index_to_add = np.random.randint(len(individual) + 1)
-                individual.insert(index_to_add, (first, second))
-        new_population.append(individual)
-
-    crossover_indices = [i for i in range(len(new_population)) if np.random.rand() < CROSS_OVER_PROB]
-    if len(crossover_indices) % 2 != 0:
-        del crossover_indices[-1]
-    for i in range(0, len(crossover_indices), 2):
-        idx_1, idx_2 = crossover_indices[i], crossover_indices[i + 1]
-        cut_point = np.random.randint(1, min(len(new_population[idx_1]), len(new_population[idx_2])))
-        new_ind_1 = new_population[idx_1][:cut_point] + new_population[idx_2][cut_point:]
-        new_ind_2 = new_population[idx_2][:cut_point] + new_population[idx_1][cut_point:]
-        new_population[idx_1], new_population[idx_2] = copy.deepcopy(new_ind_1), copy.deepcopy(new_ind_2)
-    return new_population
+def selection(population: list, fitness_values: np.ndarray,
+              strategy: Strategy = RankBased()) -> list:
+    return strategy(population, fitness_values)
 
 
-def upgrade_input(input_population: list) -> list:
-    new_population = list()
-    for individual in input_population:
-        new_population.append(np.where(np.random.rand(NETWORK_SIZE) < MUTATION_PROB, 1 - individual, individual))
-
-    crossover_indices = [i for i in range(len(new_population)) if np.random.rand() < CROSS_OVER_PROB]
-    if len(crossover_indices) % 2 != 0:
-        del crossover_indices[-1]
-    for i in range(0, len(crossover_indices), 2):
-        idx_1, idx_2 = crossover_indices[i], crossover_indices[i + 1]
-        cut_point = NETWORK_SIZE // 2
-        new_ind_1 = np.concatenate([new_population[idx_1][:cut_point], new_population[idx_2][cut_point:]])
-        new_ind_2 = np.concatenate([new_population[idx_2][:cut_point], new_population[idx_1][cut_point:]])
-        new_population[idx_1], new_population[idx_2] = copy.deepcopy(new_ind_1), copy.deepcopy(new_ind_2)
-    return new_population
-
-
-def selection(population: list, fitness_values: list, elitism_nr=0, base=0.95) -> list:
-    sorted_indices = np.argsort(fitness_values)[::-1]
-    inverse_perm = np.argsort(sorted_indices)
-    new_fitness = base ** np.arange(sorted_indices.size)
-    new_fitness = new_fitness[inverse_perm]
-    total_fitness = sum(new_fitness)
-    individual_probabilities = [fitness_val / total_fitness for fitness_val in new_fitness]
-    cumulative_probabilities = np.cumsum(individual_probabilities)
-    if not elitism_nr:
-        r = np.random.rand(POP_SIZE)
-        selected = np.searchsorted(cumulative_probabilities, r)
-        new_population = [population[idx] for idx in selected]
-    else:
-        best_fitness_values = sorted(fitness_values, reverse=True)[:elitism_nr]
-        chosen_elitism_values = [np.where(fitness_values == i)[0][0] for i in best_fitness_values]
-        r = np.random.rand(POP_SIZE - elitism_nr)
-        selected = np.searchsorted(cumulative_probabilities, r)
-        new_population = [population[idx] for idx in selected]
-        new_population.extend([population[idx] for idx in chosen_elitism_values])
-    return new_population
+def generate_all_inputs():
+    # # Carteian product of {0,1} ^ NETWORK_SIZE
+    # domain = np.repeat(np.array([[0, 1]]), NETWORK_SIZE, axis=0)
+    # return np.array(
+    #     np.meshgrid(*domain)
+    # ).T.reshape(-1, NETWORK_SIZE)
+    return [np.array(tup) for tup in itertools.product(*[[0, 1] for _ in range(NETWORK_SIZE)])]
 
 
 def main():
+    setup_logging(log_level=logging.DEBUG)
+    context = PlotContext(2,
+                          title='Mean network fitness (relative and absolute)',
+                          labels=['Mean relative network fitness',
+                                  'Mean absolute network fitness'],
+                          colors=['blue', 'green'],
+                          line_styles=['--', '--'])
+    logger = logging.getLogger('general')
+    columns = ['net_max', 'net_mean', 'net_std',
+               'input_max', 'input_mean', 'input_std',
+               'test_max', 'test_mean', 'test_std']
+    fitness_stats = pd.DataFrame(columns=columns)
+
     start_time = time.time()
     population = generate_population()
     input_population = generate_input_population()
-    fitness_values = fitness_networks(population, input_population)
-    input_fitness_values = fitness_input(input_population, population)
-    best_val, best_individual = get_best_individual(population, fitness_values)
-    best_input_val, best_input_individual = get_best_individual(input_population, input_fitness_values)
-    fp = open('log.txt', 'w')
-    for i in range(NR_EPOCHS):
-        if (i + 1) % 10 == 0:
-            fp.flush()
-        print(f'Current epoch: {i}', file=fp)
-        print(f'Current epoch: {i}')
-        population = selection(population, fitness_values, elitism_nr=ELITISM_NR)
-        input_population = selection(input_population, input_fitness_values)
+    test_input_population = generate_all_inputs()
+    net_fitness, input_fitness = evaluate(population, input_population,
+                                          multiprocessing=True)
+    test_net_fitness, _ = evaluate(population, test_input_population,
+                                    multiprocessing=True)
+    collect_and_log_fitness(net_fitness, input_fitness,
+                            test_population_fit=test_net_fitness,
+                            collector=fitness_stats)
+    best_val, best_individual = get_best_individual(population, net_fitness)
+    best_input_val, best_input_individual = get_best_individual(input_population, input_fitness)
+    context.plot(0, (fitness_stats['net_mean'], fitness_stats['test_mean']))
+    for i in range(1, N_EPOCHS+1):
+        print(f'Epoch {i}/{N_EPOCHS}')
+        logger.info('Current epoch: %d', i)
+        population = selection(population, net_fitness, strategy=Roulette())
+        input_population = selection(input_population, input_fitness,
+                                     strategy=Roulette())
         population = upgrade(population)
         input_population = upgrade_input(input_population)
-        fitness_values = fitness_networks(population, input_population)
-        input_fitness_values = fitness_input(input_population, population)
-        new_best_val, new_best_individual = get_best_individual(population, fitness_values)
-        new_best_input_val, new_best_input_individual = get_best_individual(input_population, input_fitness_values)
-        print(f'Current best: {best_val}', file=fp)
-        print(f'New best: {new_best_val}', file=fp)
-        print(f'Current input best: {best_input_val}', file=fp)
-        print(f'New input best: {new_best_input_val}', file=fp)
+        net_fitness, input_fitness = evaluate(population, input_population,
+                                              multiprocessing=True)
+        test_net_fitness, _ = evaluate(population, test_input_population,
+                                       multiprocessing=True)
+        collect_and_log_fitness(net_fitness, input_fitness,
+                                test_population_fit=test_net_fitness,
+                                collector=fitness_stats)
+        new_best_val, new_best_individual = get_best_individual(population, net_fitness)
+        new_best_input_val, new_best_input_individual = get_best_individual(input_population, input_fitness)
+        context.plot(i, (fitness_stats['net_mean'], fitness_stats['test_mean']))
         if new_best_val > best_val:
             best_val = new_best_val
             best_individual = new_best_individual
         if new_best_input_val > best_input_val:
             best_input_val = new_best_input_val
             best_input_individual = new_best_input_individual
-    print(f'The best loss was {best_val}!', file=fp)
-    print(f'The best loss was {best_val}!')
-    print(f'Best individual: {best_individual}', file=fp)
-    print(f'Best individual: {best_individual}')
-    print(f'Best input individual: {best_input_individual}', file=fp)
-    print(f'Best input individual: {best_input_individual}')
-    print(f'Time taken: {time.time() - start_time} seconds!', file=fp)
-    print(f'Time taken: {time.time() - start_time} seconds!')
-    fp.close()
+    logger.info('Time elapsed: %f', time.time() - start_time)
+    plt.waitforbuttonpress()
 
 
 if __name__ == '__main__':
     main()
+
--- Lab_1/utils.py
+++ Lab_1/utils.py
@@ -1,4 +1,11 @@
 import itertools
+import logging
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+from ga_defines import *
 
 
 def compare_swap(lst, i, j):
@@ -16,3 +23,123 @@ def outputs(net, n):
     for sortee in sortees:
         sort(net, sortee)
     return set(map(tuple, sortees))
+
+
+class DispatchingFormatter:
+    def __init__(self, formatters, default_formatter):
+        self.__formatters = formatters
+        self.__default_formatter = default_formatter
+
+    def format(self, record):
+        logger = logging.getLogger(record.name)
+        while logger:
+            if logger.name in self.__formatters:
+                formatter = self.__formatters[logger.name]
+                break
+            else:
+                logger = logger.parent
+        else:
+            # No formatter found, use default:
+            formatter = self.__default_formatter
+        return formatter.format(record)
+
+
+class PlotContext:
+    def __init__(self, n_plots, title=None, labels=None,
+                 colors=None, line_styles=None):
+        self.n_plots = n_plots
+        self.title = title
+        # Configure pyplot figure and axes objects
+        self.fig, self.ax = self.__setup_plot(n_plots, labels, colors, line_styles)
+        self.__configure_legend()
+
+    def __setup_plot(self, n_plots, labels, colors, line_styles):
+        plt.ion()
+        fig, ax = plt.subplots()
+        ax.set_xlim(0, N_EPOCHS)
+        ax.set_ylim(0.0, 1.0)
+        n_c = len(colors) if colors is not None else 0
+        n_ls = len(line_styles) if line_styles is not None else 0
+        n_labels = len(labels) if labels is not None else 0
+        # Initialize lineplots
+        for i in range(n_plots):
+            ls = line_styles[i] if 0 <= i < n_ls else None
+            c = colors[i] if 0 <= i < n_c else None
+            label = labels[i] if 0 <= i < n_labels else None
+            ax.plot([0], [0], linestyle=ls, color=c, label=label)
+        ax.legend()
+        ax.set_title(self.title)
+        fig.canvas.draw_idle()
+        return fig, ax
+
+    def __configure_legend(self):
+        pass
+
+    def plot(self, i, data):
+        new_x = np.arange(i+1)
+        lines = self.ax.get_lines()
+        for idx, new_y in enumerate(data):
+            lines[idx].set_data(new_x, new_y)
+        self.fig.canvas.draw_idle()
+        plt.pause(0.01)
+
+
+def setup_logging(log_level=logging.INFO):
+    log_handler = logging.FileHandler('log.txt', mode='w', encoding='utf-8')
+    log_handler.setLevel(log_level)
+    log_handler.setFormatter(DispatchingFormatter({
+            'general': logging.Formatter('%(levelname)s: %(asctime)s - %(message)s'),
+            'general.array': logging.Formatter('')
+        },
+        logging.Formatter('%(levelname)s: %(message)s')))
+    logging.getLogger().addHandler(log_handler)
+    logging.getLogger().setLevel(log_level)
+
+
+def get_statistics(network_fit, input_fit, test_population_fit=None):
+    result = ((network_fit.max(), network_fit.mean(), network_fit.std()),
+              (input_fit.max(), input_fit.mean(), input_fit.std()))
+    if test_population_fit is not None:
+        result += ((test_population_fit.max(),
+                    test_population_fit.mean(),
+                    test_population_fit.std()),)
+    return result
+
+def collect_and_log_fitness(network_fit, input_fit,
+                            test_population_fit=None, collector=None):
+    log = logging.getLogger('general')
+    array_log = logging.getLogger('general.array')
+    stats = get_statistics(network_fit, input_fit,
+                           test_population_fit=test_population_fit)
+    net_max, net_mean, net_std = stats[0]
+    input_max, input_mean, input_std = stats[1]
+    log.debug('Network fitness:')
+    array_log.debug(network_fit)
+    log.info('Network best: %f', net_max)
+    log.info('Mean network fitness: %f', net_mean)
+    log.info('Std. dev. network fitness: %f', net_std)
+    log.info('Input best: %f', input_max)
+    log.info('Mean input fitness: %f', input_mean)
+    log.info('Std. dev. input fitness: %f', input_std)
+    if test_population_fit is not None:
+        test_max, test_mean, test_std = stats[2]
+        log.debug('Network fitness (on test input):')
+        array_log.debug(test_population_fit)
+        log.info('Network best (on test input): %f', test_max)
+        log.info('Mean network fitness (on test input): %f', test_mean)
+        log.info('Std. dev. network fitness (on test input): %f', test_std)
+    if collector is not None:
+        if isinstance(collector, dict):
+            collector['net_max'].append(net_max)
+            collector['net_mean'].append(net_mean)
+            collector['net_std'].append(net_std)
+            collector['input_max'].append(input_max)
+            collector['input_mean'].append(input_mean)
+            collector['input_std'].append(input_std)
+            if test_population_fit is not None:
+                collector['test_max'].append(test_max)
+                collector['test_mean'].append(test_mean)
+                collector['test_std'].append(test_std)
+        if isinstance(collector, pd.DataFrame):
+            row = np.array([s for stat in stats for s in stat])
+            collector.loc[collector.shape[0]] = row
